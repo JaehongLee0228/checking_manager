c281067cdcf28b12ca9991506d798e8d7ddc0276
Yeonwoo <dth04019@naver.com>
2020-10-01 02:48:57 +0900

    QR코드 공유

--- app/src/main/java/com/checking_manager/checking_manager/reQRPage.java
+++ app/src/main/java/com/checking_manager/checking_manager/reQRPage.java
@@ -1,9 +1,17 @@
 package com.checking_manager.checking_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
@@ -12,11 +20,18 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class reQRPage extends AppCompatActivity {
 
     private String group_name;
     private String pos;
     private String stuff_name;
+
+    private Button shareBtn;
+    private ImageView IV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +44,7 @@ public class reQRPage extends AppCompatActivity {
 
         String sen = group_name + "_" + pos + "_" + stuff_name;
 
-        ImageView IV = (ImageView)findViewById(R.id.qrCode);
+        IV = (ImageView)findViewById(R.id.qrCode);
 
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try{
@@ -40,5 +55,47 @@ public class reQRPage extends AppCompatActivity {
         } catch (Exception e) {
 
         }
+
+        shareBtn = (Button)findViewById(R.id.shareBtn);
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bitmap bitmap = getBitmapFromView(IV);
+                try{
+                    File file = new File(getApplication().getExternalCacheDir(), "logicchip.png");
+
+                    FileOutputStream fOut = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 10, fOut);
+                    fOut.flush();
+                    fOut.close();
+                    file.setReadable(true, false);
+                    final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(getBaseContext(), "com.checking_manager.checking_manager.fileprovider", file));
+
+                    intent.setType("image/png");
+                    startActivity(Intent.createChooser(intent, "Share Image via"));
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getWidth(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable = view.getBackground();
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas);
+        }else{
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return returnedBitmap;
     }
 }
