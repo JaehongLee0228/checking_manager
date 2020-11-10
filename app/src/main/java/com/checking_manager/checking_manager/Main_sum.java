d4c8311fb482d9b282744b5ab964546109c8681a
Wijinhyun <maeho1459@naver.com>
2020-11-10 15:44:49 +0900

    Viewpager Title update

--- app/src/main/java/com/checking_manager/checking_manager/Main_sum.java
+++ app/src/main/java/com/checking_manager/checking_manager/Main_sum.java
@@ -3,18 +3,27 @@ package com.checking_manager.checking_manager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.checking_manager.checking_manager.login.log_in;
+import com.checking_manager.checking_manager.login.sign_up_activity;
 import com.google.android.material.tabs.TabLayout;
 
 public class Main_sum extends AppCompatActivity {
 
     private FragmentPagerAdapter fragmentPagerAdapter;
     public static Class returnClass;
+    private String group_name;
+    private Button Btn_back;
+    private TextView Tv_title;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +37,18 @@ public class Main_sum extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         viewPager.setAdapter(fragmentPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        Btn_back = findViewById(R.id.btn_back);
+        Tv_title = findViewById(R.id.tv_title);
+        group_name = getIntent().getStringExtra("group_name");
+        Tv_title.setText(group_name);
+
+        Btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
 
 
